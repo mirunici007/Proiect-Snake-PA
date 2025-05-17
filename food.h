dabf0e1c3769016e3b8f73deb2f894e10cc08019
mirunici007 <mirunasardaru15@gmail.com>
2025-05-17 18:39:23 +0300

    mici modificari

--- food.h
+++ food.h
@@ -10,6 +10,8 @@
 
 #define CELL_SIZE 20  // dimensiunea unui pătrat din grid, în pixeli
 
+
+
 typedef enum {
     FOOD_1P = 2,
     FOOD_2P = 3,
@@ -20,8 +22,7 @@ typedef struct food_node {
     FOOD_TYPE type;
     struct food_node* next; 
 } FOOD;
-
-FOOD* food_list = NULL;
+extern FOOD* food_list;
 
 void spawn_food_1p(SNAKE *snake, int *x, int *y);
 void spawn_food_2p(SNAKE *snake, int *x, int *y);
